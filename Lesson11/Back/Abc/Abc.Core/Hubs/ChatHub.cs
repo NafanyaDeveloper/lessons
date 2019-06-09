7368719d434662ab0ea3b84f20081121eccea52f
Vlad <hello@world.test>
2019-06-09 21:27:52 +0300

    initial

---
+++ Lesson11/Back/Abc/Abc.Core/Hubs/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abc.Core.Hubs
+{
+    public class ChatHub: Hub
+    {
+    }
+}
+
